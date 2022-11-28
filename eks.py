9fd0dc895247fe2d31c98d8517dd133d0af7493d
Sergey Vodwood <sergey.vodwood@gmail.com>
2022-11-29 00:05:41 +0200

    add flux bootstrap

---
+++ eks.py
@@ -0,0 +1,247 @@
+from pulumi_aws import iam, ec2, eks, config, cloudwatch
+import pulumi_eks as eks_provider
+from pulumi import export, ResourceOptions
+import pulumi_kubernetes as k8s
+
+from settings import general_tags, flux_github_repo_owner, flux_github_repo_name, flux_github_token, flux_cli_version
+from vpc import demo_vpc, demo_private_subnets, demo_eks_cp_subnets
+from helpers import create_iam_role, create_oidc_role, create_policy
+
+"""
+Shared EKS resources
+"""
+cluster_descriptor = "demo-k8s"
+
+# Create an EKS cluster role
+eks_iam_role_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
+    "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+]
+
+eks_iam_role = create_iam_role(f"{cluster_descriptor}-eks-role", "Service", "eks.amazonaws.com", eks_iam_role_policy_arns)
+
+# Create a default node role for Karpenter
+karpenter_default_nodegroup_role_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+]
+
+# Create a default Fargate execution role:
+default_fargate_pod_execution_role_policy = [
+    "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+]
+
+# Create a default demo EKS cluster nodegroup security group:
+demo_nodegroup_security_group = ec2.SecurityGroup(f"custom-node-attach-{cluster_descriptor}",
+    description=f"{cluster_descriptor} custom node security group",
+    vpc_id=demo_vpc.id,
+    tags={**general_tags, "Name": f"custom-node-attach-{cluster_descriptor}", "karpenter.sh/discovery": f"{cluster_descriptor}"}
+)
+
+demo_nodegroup_security_group_inbound_custom_cidrs = ec2.SecurityGroupRule(f"inbound-eks-node-{cluster_descriptor}",
+    type="ingress",
+    from_port=443,
+    to_port=443,
+    protocol="tcp",
+    cidr_blocks=["0.0.0.0/0"],
+    security_group_id=demo_nodegroup_security_group.id
+)
+
+demo_nodegroup_security_group_oubound_custom_cidrs = ec2.SecurityGroupRule(f"outbound-eks-node-{cluster_descriptor}",
+    type="egress",
+    to_port=0,
+    protocol="-1",
+    from_port=0,
+    cidr_blocks=["0.0.0.0/0"],
+    security_group_id=demo_nodegroup_security_group.id
+)
+
+# Create a default demo EKS cluster security group:
+demo_cluster_security_group = ec2.SecurityGroup(f"custom-cluster-attach-{cluster_descriptor}",
+    description=f"{cluster_descriptor} custom security group",
+    vpc_id=demo_vpc.id,
+    tags={**general_tags, "Name": f"custom-cluster-attach-{cluster_descriptor}"}
+)
+
+demo_cluster_security_group_inbound_custom_cidrs = ec2.SecurityGroupRule(f"inbound-eks-cp-{cluster_descriptor}",
+    type="ingress",
+    from_port=443,
+    to_port=443,
+    protocol="tcp",
+    cidr_blocks=["0.0.0.0/0"],
+    security_group_id=demo_cluster_security_group.id
+)
+
+demo_cluster_security_group_oubound_custom_cidrs = ec2.SecurityGroupRule(f"outbound-eks-cp-{cluster_descriptor}",
+    type="egress",
+    to_port=0,
+    protocol="-1",
+    from_port=0,
+    cidr_blocks=["0.0.0.0/0"],
+    security_group_id=demo_cluster_security_group.id
+)
+
+# Create a default Karpenter node role and instance profile:
+karpenter_node_role = create_iam_role(f"KarpenterNodeRole-{cluster_descriptor}", "Service", "ec2.amazonaws.com", karpenter_default_nodegroup_role_policy_arns)
+karpenter_instance_profile = iam.InstanceProfile(f"KarpenterNodeInstanceProfile-{cluster_descriptor}",
+    role=karpenter_node_role.name,
+    name=f"KarpenterNodeInstanceProfile-{cluster_descriptor}"
+)
+
+# Create a Fargate profile service role:
+fargate_profile_service_role = create_iam_role(f"{cluster_descriptor}-fargate-role", "Service", "eks-fargate-pods.amazonaws.com", default_fargate_pod_execution_role_policy)
+
+# Create an EKS log group:
+demo_eks_loggroup = cloudwatch.LogGroup("demo-eks-loggroup", 
+    name=f"/aws/eks/{cluster_descriptor}/cluster",
+    tags=general_tags,
+    retention_in_days=1
+)
+
+# Create the cluster control plane and Fargate profiles:
+demo_eks_cluster = eks_provider.Cluster(f"eks-{cluster_descriptor}",
+    name=f"{cluster_descriptor}",
+    vpc_id=demo_vpc.id,
+    instance_role=karpenter_node_role,
+    cluster_security_group=demo_cluster_security_group,
+    create_oidc_provider=True,
+    version="1.24",
+    instance_profile_name=karpenter_instance_profile,
+    skip_default_node_group=True,
+    service_role=eks_iam_role,
+    provider_credential_opts=eks_provider.KubeconfigOptionsArgs(
+        profile_name=config.profile,
+    ),
+    endpoint_private_access=True,
+    endpoint_public_access=True,
+    enabled_cluster_log_types=["api", "audit", "authenticator", "controllerManager", "scheduler"],
+    public_access_cidrs=["0.0.0.0/0"],
+    subnet_ids=[s.id for s in demo_eks_cp_subnets],
+    tags={**general_tags, "Name": f"{cluster_descriptor}"},
+    fargate=eks.FargateProfileArgs(
+        cluster_name=f"{cluster_descriptor}",
+        subnet_ids=[s.id for s in demo_private_subnets],
+        pod_execution_role_arn=fargate_profile_service_role.arn,
+        selectors=[{"namespace": "karpenter"}, {"namespace": "flux-system"}, {"namespace": "kube-system"}]),
+    opts=ResourceOptions(depends_on=[
+            demo_nodegroup_security_group,
+            eks_iam_role,
+            demo_eks_loggroup
+        ]))
+
+demo_eks_cluster_oidc_arn = demo_eks_cluster.core.oidc_provider.arn
+demo_eks_cluster_oidc_url = demo_eks_cluster.core.oidc_provider.url
+
+# Install VPC CNI addon when the cluster is initialized:
+vpc_cni_addon = eks.Addon("vpc-cni-addon",
+    cluster_name=f"{cluster_descriptor}",
+    addon_name="vpc-cni",
+    addon_version="v1.11.4-eksbuild.1",
+    resolve_conflicts="OVERWRITE",
+    opts=ResourceOptions(
+        depends_on=[demo_eks_cluster]
+    )
+)
+
+# Install kube-proxy addon when the cluster is initialized:
+kube_proxy_addon = eks.Addon("kube-proxy-addon",
+    cluster_name=f"{cluster_descriptor}",
+    addon_name="kube-proxy",
+    addon_version="v1.24.7-eksbuild.2",
+    resolve_conflicts="OVERWRITE",
+    opts=ResourceOptions(
+        depends_on=[demo_eks_cluster, vpc_cni_addon]
+    )
+)
+
+# Install CoreDNS addon when the cluster is initialized:
+core_dns_addon = eks.Addon("coredns-addon",
+    cluster_name=f"{cluster_descriptor}",
+    addon_name="coredns",
+    addon_version="v1.8.7-eksbuild.3",
+    resolve_conflicts="OVERWRITE",
+    opts=ResourceOptions(
+        depends_on=[demo_eks_cluster, vpc_cni_addon, kube_proxy_addon]
+    )
+)
+
+# Create a kubernetes provider
+role_provider = k8s.Provider(f"{cluster_descriptor}-kubernetes-provider",
+    kubeconfig=demo_eks_cluster.kubeconfig,
+    opts=ResourceOptions(depends_on=[demo_eks_cluster])
+)
+
+# Create a service account and cluster role binding for flux controller
+
+flux_service_account = k8s.core.v1.ServiceAccount("flux-controller-service-account",
+    api_version="v1",
+    kind="ServiceAccount",
+    metadata=k8s.meta.v1.ObjectMetaArgs(
+        name="flux-controller",
+        namespace="kube-system"
+    ),
+    opts=ResourceOptions(
+        provider=role_provider,
+        depends_on=[demo_eks_cluster]
+    )
+)
+
+flux_controller_cluster_role_binding = k8s.rbac.v1.ClusterRoleBinding("flux-controller-sa-crb",
+    role_ref=k8s.rbac.v1.RoleRefArgs(
+        api_group="rbac.authorization.k8s.io",
+        kind="ClusterRole",
+        name="cluster-admin"
+    ),
+    subjects=[
+        k8s.rbac.v1.SubjectArgs(
+            kind="ServiceAccount",
+            name="flux-controller",
+            namespace="kube-system"
+        )
+    ],
+    opts=ResourceOptions(
+        provider=role_provider,
+        depends_on=[demo_eks_cluster]
+    )
+)
+
+# Create the bootstrap job for flux-cli
+flux_bootstrap_job = k8s.batch.v1.Job("fluxBootstrapJob",
+    opts=ResourceOptions(
+        provider=role_provider,
+        depends_on=[flux_service_account, flux_controller_cluster_role_binding]
+    ),
+    metadata=k8s.meta.v1.ObjectMetaArgs(
+        name="flux-bootstrap-job",
+        namespace="kube-system",
+        annotations={"pulumi.com/replaceUnready": "true"}
+    ),
+    spec=k8s.batch.v1.JobSpecArgs(
+        backoff_limit=3,
+        template=k8s.core.v1.PodTemplateSpecArgs(
+            spec=k8s.core.v1.PodSpecArgs(
+                service_account_name="flux-controller",
+                containers=[k8s.core.v1.ContainerArgs(
+                    env=[k8s.core.v1.outputs.EnvVar(
+                        name="GITHUB_TOKEN",
+                        value=flux_github_token
+                    )],
+                    command=[
+                        "flux",
+                        "bootstrap",
+                        "github",
+                        f"--owner={flux_github_repo_owner}",
+                        f"--repository={flux_github_repo_name}",
+                        f"--path=./clusters/{cluster_descriptor}",
+                        "--private=false",
+                        "--personal=true"
+                    ],
+                    image=f"fluxcd/flux-cli:v{flux_cli_version}",
+                    name="flux-bootstrap",
+                )],
+                restart_policy="OnFailure",
+            ),
+        ),
+    ))
--- settings.py
+++ settings.py
@@ -35,3 +35,11 @@ demo_eks_cp_subnet_cidrs = [
 
 deployment_region = config.region
 endpoint_services = ["ecr.api","ecr.dkr","ec2","sts","logs","s3","email-smtp","cloudformation"]
+
+"""
+Flux Bootstrap args
+"""
+flux_github_repo_owner = stack_config.require("flux-github-repo-owner")
+flux_github_repo_name = stack_config.require("flux-github-repo-name")
+flux_cli_version = "0.37.0"
+flux_github_token = stack_config.require_secret("flux-github-token")
